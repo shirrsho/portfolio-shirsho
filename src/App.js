0b221d3713a418b8ffc88c4b1a552093bf4b245e
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-11-30 12:11:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+            <div className="max-width">
+                <div className='logo'><a href="#">Portfo<span>lio</span></a></div>
+                <ul className="menu">
+                    <li><a href="#home" className="menu-btn">Home</a></li>
+                    <li><a href="#about" className="menu-btn">About</a></li>
+                    <li><a href="#services" className="menu-btn" >Services</a></li>
+                    <li><a href="#skills" className="menu-btn">Skills</a></li>
+                    <li><a href="#projects" className="menu-btn">Projects</a></li>
+                    <li><a href="#gallery" className="menu-btn">Gallery</a></li>
+                </ul>
+                <div className="menu-btn">
+                    <i className="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
+
+        <section className="home" id="home">
+            <div className="max-width">
+                <div className="home-content">
+                    {/* <img src="Media/20220615_153107.jpg" alt='bgimage'/> */}
+                    <div className="text-1">Hello, my name is</div>
+                    <div className="text-2">Hasnain Iqbal Shirsho</div>
+                    <div className="text-3">& I am a <span className="typing"></span></div>
+                </div>
+            </div>
+        </section>
+
+
     </div>
   );
 }
